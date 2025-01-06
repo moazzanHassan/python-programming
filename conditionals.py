f54c71dafe38569819ea6520a05361df747071c2
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-01-06 16:47:13 +0500

    conditional

--- conditionals.py
+++ conditionals.py
@@ -4,3 +4,9 @@
 #logic operator in python (and) (or) (not)
 # Write a program to check whether a number entered by the user is even or odd.
 # Write a program to check if a person is eligible to vote. (Eligibility: The person's age must be 18 or above.)
+
+number = int(input("enter a number :"))
+if number%2 == 0:
+    print("it a even number")
+else :
+    print("the number is odd")   
