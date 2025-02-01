0386b84516407ada17ac10584c4b908048c49a49
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-02-01 23:56:12 +0500

    reverse

---
+++ lists.py
@@ -0,0 +1,7 @@
+x = [1]
+b = [2]
+c = x + b
+y = "spare"
+x.append("hello")
+
+print(len(c))
---
+++ proble.py
@@ -0,0 +1,13 @@
+# nums = [0,1,3,5]
+# dic = {}
+# lent = range(len(nums)+1)
+# for num in nums:
+#     if num not in dic:
+#         dic[num] = 1
+# for i in lent:
+#     if i not in dic:
+#         print(i)
+
+x = "this is a string"
+
+print(x[::-1])
