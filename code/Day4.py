ec6c9e90b458ebb0996d4e867a3295a0e0a24a24
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-03-04 11:07:19 +0500

    Day5

---
+++ code/Day4.py
@@ -0,0 +1,6 @@
+arr = [1,2,3]
+arr2 =[]
+for i in range(2):
+    for a in arr:
+        arr2.append(a)
+print(arr2)
---
+++ code/Day5.py
@@ -0,0 +1,4 @@
+str = ["       hell world   "]
+print(str[0])
+str2 = str[0].strip()
+print(str2)
