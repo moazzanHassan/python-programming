d22e43a9f63ab9ff599ec795b1ed00587151da35
Moazzan Hassan <moazzanhassan907@gmail.com>
2025-03-12 19:13:05 +0500

    it's day 11

--- code/Day10.py
+++ code/Day10.py
@@ -1,46 +1,11 @@
-'''Exercise 4: Counter Class
-Task: Ek Counter class banao jo:
+from collections import Counter
 
-Ek count variable store kare jo shuru me 0 ho
-increment() aur decrement() methods likho
-get_count() method likho jo current count return kare'''
+str = "wow"
 
-class counter:
-    def __init__(self):
-        self.counter = 0
-    def decrement(self,dec):
-        self.counter = self.counter-dec
-    def increment(self,inc):
-        self.counter = self.counter + inc
-    def get_count(self):
-        return self.counter
+s = Counter(str)
 
-# count = counter()
-# count.increment(1000)
-# print(count.get_count())
-# count.decrement(10)
-# print(count.get_count())
+print(s)
+str =["a","b","c"]
 
-'''Exercise 5: Student Grade System
-Task: Ek Student class banao jo:
-
-name aur marks store kare
-add_marks(subject, marks) method likho jo ek subject ke marks store kare
-get_average() method likho jo sab subjects ke marks ka average return kare'''
-
-class Student:
-    def __init__(self,name,sub_mar):
-        self.name = name
-        sub_mar ={
-            sub: [],
-            mark : [] 
-
-        }
-    def add_marks(self):
-        
-        self.sub_mar[self.sub].append(self.sub)
-        self.sub_mar[self.semark].append(self.mark)
-    def get_average(self):
-        pass
-
-
+s= "".join(str)
+print(s)
---
+++ code/Day11.py
@@ -0,0 +1,6 @@
+int1 = input("enter a number")
+int2 = input("enter a 2nd number")
+
+sum = int(int1) + int(int2)
+
+print(sum)
